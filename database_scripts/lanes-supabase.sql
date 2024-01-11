c423b7f93be142a13a41bf6e56d987b203fea5dd
mr_ryan <61605919+RexforNext22@users.noreply.github.com>
2024-01-11 10:33:58 -0700

    Added by Ryan

--- database_scripts/lanes-supabase.sql
+++ database_scripts/lanes-supabase.sql
@@ -1,21 +1,12 @@
--- This file is for MySQL
--- Author Ryan Pinkney
-
--- Drop the database if it exists
-DROP DATABASE IF EXISTS lanes;
-
--- Create the database
-CREATE DATABASE lanes;
-
--- Switch to the newly created database
-USE lanes;
+-- This script is for Postgres sql on supabase
+-- Ryan Pinkney
 
 -- Drop the 'loads' table if it exists
 DROP TABLE IF EXISTS loads;
 
 -- Create the 'loads' table
 CREATE TABLE loads (
-    load_id INT AUTO_INCREMENT PRIMARY KEY,
+    load_id SERIAL PRIMARY KEY,
     description VARCHAR(255),
     pickup_location VARCHAR(255),
     delivery_location VARCHAR(255),
